21a8257372921f89b98d5fe43143d0ad68cd02ba
Aya Sato <ayasatography@icloud.com>
2021-02-07 20:10:11 -0500

    Updated Header and Hero, Added GlobalStyles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import { themeVars } from "./GlobalStyles";
 
 import { FiMenu, FiShoppingCart } from 'react-icons/fi';
 
@@ -7,41 +8,62 @@ import Logo from './Logo';
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 80px;
+  height: 93px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 10px;
+  flex-direction: column;
 `;
 
 const HeaderBtn = styled.button`
   background: none;
   border: none;
-
+  outline: none;
+  margin-right: 10px;
+  
   &:active {
     transform: scale(1.1);
   }
 `;
 
+const Catchline = styled.div`
+  font-size: 12px;
+  font-weight: 700;
+  margin: 0 0 0 18px;
+`;
+
+const firstRowStyle = {
+  display: 'flex', 
+  justifyContent: 'space-between', 
+  alignItems: 'center',
+  position: 'relative',
+  top: '5px',
+  marginLeft: '10px'
+}
+
 const cartStyle = {
   fontSize: '35px',
-  marginRight: '5px', 
-  color: '#13A4E7'
+  color: `${themeVars.blue}`,
+  position: 'relative',
+  top: '5px'
 };
 
 const menuStyle = {
   fontSize: '35px', 
-  color: '#13A4E7'
+  color: `${themeVars.blue}`,
+  position: 'relative',
+  top: '8px'
 }
 
 const Header = () => {
   return (
     <Wrapper>
-      <Logo />
-      <div>
-        <HeaderBtn><FiShoppingCart style={cartStyle}/></HeaderBtn>
-        <HeaderBtn><FiMenu style={menuStyle}/></HeaderBtn>
+      <div style={firstRowStyle}>
+        <Logo />
+        <div>
+          <HeaderBtn><FiShoppingCart style={cartStyle}/></HeaderBtn>
+          <HeaderBtn><FiMenu style={menuStyle}/></HeaderBtn>
+        </div>
       </div>
+      <Catchline>Tech. Lifestyle. Fitness.</Catchline>
     </Wrapper>
   );
 }
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import styled, { css } from 'styled-components';
+import { themeVars } from "./GlobalStyles";
+
+import heroGif from '../assets/hero.gif';
+
+const commonWrapperCSS = css`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const TextWrapper1 = styled.div`
+  ${commonWrapperCSS};
+  height: 28px;
+  background-color: ${themeVars.black};
+  align-items: center;
+`;
+
+const TextWrapper2 = styled.div`
+  ${commonWrapperCSS};
+  height: 56px;
+  background-color: ${themeVars.midnightGreen};
+  align-items: flex-end;
+  position: relative;
+  top: -25px;
+`;
+
+const Para1 = styled.p`
+  color: ${themeVars.white};
+  font-size: 14px;
+  font-weight: 300;
+`;
+
+const Para2 = styled.p`
+  color: ${themeVars.white};
+  font-size: 20px;
+  font-weight: 700;
+  position: relative;
+  top: -10px;
+`;
+
+const Gif = styled.img`
+  width: 100%;
+  margin-top: 14px;
+`;
+
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <TextWrapper1>
+        <Para1>Free shipping on all orders over $50.</Para1>
+      </TextWrapper1>
+      <Gif src={heroGif}/>
+      <TextWrapper2>
+        <Para2>Select by body parts</Para2>
+      </TextWrapper2>
+    </>
   )
 }
 

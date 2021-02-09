1c2cf816373ff3de02b84f0c700acdd06da35b56
Zayd Ben Osmane <zayd@benosmane.com>
2021-02-09 14:28:59 -0500

    dependecies + 404 page

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import { FiFrown } from "react-icons/fi";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <p>error go back</p>
-        </div>
+        <Main>
+            <FiFrown size={90} />
+            <h1>404</h1>
+            <h4>Page not found</h4>
+        </Main>
     );
 };
 
 export default ErrorPage;
+
+const Main = styled.div`
+    height: 76vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;

311c5d3c0d4098498c27629b11f266fa9cd5b7a4
Aya Sato <ayasatography@icloud.com>
2021-02-10 00:40:55 -0500

    Created About component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,12 +4,11 @@ import LandingPage from "../pages/LandingPage";
 import Error from "../pages/404";
 import Header from "./Header";
 import Footer from "./Footer";
+import About from "./About";
 
 import GlobalStyles from "./GlobalStyles";
 
 function App() {
- 
-
   return (
     <>
       <GlobalStyles />
@@ -18,6 +17,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <LandingPage />
+          </Route>
+          <Route exact path="/about">
+            <About />
           </Route>
           <Route exact path="/error">
             <Error />
